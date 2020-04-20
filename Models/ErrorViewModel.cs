f52394b9f51c1ac07ca733075e86aa0df7054c47
Mark Hobson <mark.hobson@blackpepper.co.uk>
2020-04-20 15:46:59 +0100

    Initial mvc template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rider_section_issue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
